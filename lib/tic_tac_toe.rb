6f35ecc9a0bd47e3b8008317ac1cd1a9cbf6e5e6
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-26 17:49:12 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,42 +1,41 @@
 class TicTacToe
-
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
 
   WIN_COMBINATIONS = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [6,4,2]
-  ]
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
 
   def display_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
+    puts "---------"
+    puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
+    puts "---------"
+    puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
   end
 
-  def move(location, character = "X")
-    @board[location.to_i - 1] = character
+  def move(index, character = "X")
+    @board[index] = current_player
   end
 
   def position_taken?(position)
-    if @board[position] == "X" || @board[position] == "O"
-      true
-    else
-      false
-    end
-  end
+   if @board[position] == "X" || @board[position] == "O"
+     true
+   else
+     false
+   end
+ end
 
-  def valid_move?(position)
-    position = position.to_i - 1
+ def valid_move?(position)
+   position = position.to_i - 1
     if position.between?(0,8) && !position_taken?(position)
       true
     else
@@ -46,33 +45,32 @@ class TicTacToe
 
   def turn
     puts "Please enter 1-9:"
-    input = gets.strip
+  input = gets.strip
     if valid_move?(input)
       move(input, current_player)
+      display_board
     else
       turn
     end
-    display_board
   end
+
 
   def turn_count
-    counter = 0
-    @board.each do |i|
-      if i == "X" || i == "O"
-        counter += 1
-      end
-    end
-    counter
-  end
-
-  def current_player
-    turn_count % 2 == 0 ? "X" : "O"
-  end
+   counter = 0
+   @board.each do |i|
+     if i == "X" || i == "O"
+       counter += 1
+     end
+   end
+   counter
+ end
 
 
+ def current_player
+   turn_count % 2 == 0 ? "X" : "O"
+ end
 
-  def won?
-
+ def won?
     board_empty = @board.none? { |i| i == "X" || i = "O"}
     if board_empty
       false
@@ -87,7 +85,7 @@ class TicTacToe
 end
 
   def full?
-    @board.all? { |i| i =="X" || i == "O"}
+    @board.all? { |i| i == "X" || i == "O"}
   end
 
   def draw?
@@ -111,14 +109,15 @@ end
   end
 
   def play
-    until over?
-      turn
-    end
-
-    if won?
-      puts "Congratulations #{winner}!"
-    elsif draw?
-      puts "Cats Game!"
-    end
+  until over?
+    turn
   end
+
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
+
 end
